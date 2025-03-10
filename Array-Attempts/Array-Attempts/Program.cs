b815de8f11a7447ecfee537862e3310a30cf5f0b
ibad-cr <ismayilovibad47@gmail.com>
2025-03-11 01:02:10 +0400

    array-attempts

---
+++ Array-Attempts/Array-Attempts/Program.cs
@@ -0,0 +1,137 @@
+﻿namespace Array_Attempts
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Adding a new element to an array
+
+            //int[] number = { 1, 2, 3, 4 };
+            //int newItem = 5;
+
+            //int[] newArray = new int[number.Length + 1];
+
+            //for (int i = 0; i < number.Length; i++)
+            //{
+            //    newArray[i] = number[i];
+            //}
+
+            //newArray[newArray.Length - 1] = newItem;
+
+            //Console.WriteLine("New Array");
+            //foreach (var item in newArray)
+            //{
+            //    Console.WriteLine(item);
+            //}
+
+
+
+            //Adding a new element to an array - incomplete
+
+            //int[] number = { 1, 2, 3, 4 };
+            //int newItem = 4;
+
+            //int[] newArray = new int[number.Length - 1];
+
+            //for (int i = 0; i < number.Length; i++)
+            //{
+            //    newArray[i] = number[i];
+
+            //    if (number[i] == newItem)
+            //    {
+            //        continue;
+            //    }
+
+            //    newArray[i] = newItem;
+            //}
+
+            //Console.WriteLine("Yeni massiv:");
+            //foreach (var item in newArray)
+            //{
+            //    Console.WriteLine(item);
+            //}
+
+
+            //Array reverse method
+            //int[] array = { 1, 2, 3, 4 };
+
+            //for (int i = array.Length - 1; i >= 0; i--)
+            //{
+            //    Console.WriteLine(array[i]);
+            //}
+
+
+            //Ən kiçik və Ən böyük ədədin arraydan tapılması
+            //int[] number = { 9, 5, 0, 4 };
+
+            //int min = number[0];
+            //int max = number[0];
+
+            //for (int i = 0; i < number.Length; i++)
+            //{
+            //    if (number[i] < min)
+            //    {
+            //        min = number[i];
+            //    }
+
+            //    if (number[i] > max)
+            //    {
+            //        max = number[i];
+            //    }
+            //}
+
+            //Console.WriteLine("Ən kiçik ədəd: " + min);
+            //Console.WriteLine("Ən böyük ədəd: " + max);
+
+
+            //Arrayda itemlərin kvatratlarını alıb yeni arrraya mənimsətmək;
+            //int[] number1 = { 9, 5, 0, 4 };
+            //int[] newArray = new int[number1.Length];
+
+            //for (int i = 0; i < number1.Length; i++)
+            //{
+            //    int squaring = number1[i] * number1[i];
+            //    newArray[i] = squaring;
+            //    Console.WriteLine(newArray[i]);
+            //}
+
+            //Console.WriteLine(newArray.Length);
+
+
+            //Arrayda itemlərin kvatratlarının cəmini alıb yeni arrraya mənimsətmək;
+            //int[] number1 = { 9, 5, 0, 4 };
+            //int[] newArray = new int[number1.Length];
+
+            //int sumOfSquares = 0;
+
+            //for (int i = 0; i < number1.Length; i++)
+            //{
+            //    int squaring = number1[i] * number1[i];
+            //    newArray[i] = squaring;
+            //    sumOfSquares += squaring;
+            //    Console.WriteLine(newArray[i]);
+            //}
+
+            //Console.WriteLine(newArray.Length);
+            //Console.WriteLine("Kvadratların cəmi: " + sumOfSquares);
+
+
+
+            //Arrayda təkrarlanan elemetləri tapmaq və silmək - silməyi yaza bilmədim
+            //string[] strings = { "item_1", "item_2", "item_3", "item_1" };
+
+            //for (int i = 0; i < strings.Length; i++)
+            //{
+            //    for (int j = 0; j < i; j++)
+            //    {
+            //        if( strings[j] == strings[i])
+            //        {
+
+            //            string result = strings[j];
+            //            Console.WriteLine(result);
+            //        }
+            //    }
+            //}
+        }
+    }
+}
