3a713ac65d8c81820a4c9ea0f8ee1da0dce3f483
ibad-cr <ismayilovibad47@gmail.com>
2025-05-05 23:03:22 +0400

    practica mvc

---
+++ Code-Attempts/HotelMvc/HotelMvc/Controllers/HomeController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HotelMvc.Controllers;
+
+public class HomeController : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
---
+++ Tasks/WebKlinikApp/WebKlinikApp/WebKlinikApp/Program.cs
@@ -0,0 +1,21 @@
+namespace WebKlinikApp
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+            builder.Services.AddControllersWithViews();
+
+            var app = builder.Build();
+
+            app.UseStaticFiles();
+            app.MapControllerRoute(
+                name: "Default",
+                pattern: "{Controller=Home}/{Action=Index}"
+            );
+
+            app.Run();
+        }
+    }
+}
