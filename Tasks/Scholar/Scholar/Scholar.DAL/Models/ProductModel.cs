090af4342ccc3f8d6c9cd045673a30ffd3da12d6
ibad-cr <ismayilovibad47@gmail.com>
2025-05-20 20:52:03 +0400

    Scholar

---
+++ Tasks/Scholar/Scholar/Scholar.DAL/Models/ProductModel.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Diagnostics.Tracing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Villa.DAL.Models;
+
+public class ProductModel
+{
+    public int Id { get; set; }
+    public string ImgPath { get; set; }
+    [NotMapped]
+    public IFormFile ImgFile { get; set; }
+    [Required(ErrorMessage = "Moye More")]
+    public string Category { get; set; }
+    [Required(ErrorMessage = "Moye More")]
+    public double Price { get; set; }
+    [Required(ErrorMessage = "Moye More")]
+    public string Title { get; set; }
+    [Required(ErrorMessage = "fdfMoye More")]
+    public string Desc { get; set; }
+}
---
+++ Tasks/Scholar/Scholar/Scholar.MVC/Areas/Admin/Controllers/CrudController.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Villa.BL.Services;
+using Villa.DAL.Models;
+
+namespace Villa.MVC.Areas.Admin.Controllers;
+
+[Area("Admin")]
+public class CrudController : Controller
+{
+
+    private readonly ProductService _productService;
+
+    public CrudController(ProductService productService)
+    {
+        _productService = productService;
+    }
+    public IActionResult Index()
+    {
+        var getAllProduct = _productService.GetAllProduct();
+        return View(getAllProduct);
+    }
+
+    [HttpGet]
+    public IActionResult Create()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    public IActionResult Create(ProductModel product)
+    {
+        //if (!ModelState.IsValid)
+        //{
+        //    return View();
+        //}
+
+        _productService.CreateProduct(product);
+        return RedirectToAction(nameof(Index));
+    }
+
+    [HttpGet]
+    public IActionResult Info(int id)
+    {
+        ProductModel product = _productService.GetProductById(id);
+        return View(product);
+    }
+
+    [HttpPost]
+    public IActionResult Delete(int id)
+    {
+        _productService.DeleteProduct(id);
+        return RedirectToAction(nameof(Index));
+    }
+
+    [HttpGet]
+    public IActionResult Update(int id)
+    {
+        return View();
+    }
+
+    [HttpPost]
+    public IActionResult Update(ProductModel product)
+    {
+        _productService.UpdateProduct(product);
+        return RedirectToAction(nameof(Index));
+    }
+}
